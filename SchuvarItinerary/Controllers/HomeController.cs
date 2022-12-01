92c9acc55d6c7b5c35ee2f36911cd1d1a0a4f995
Daniel Flores <dflores2t@gmail.com>
2022-12-01 16:51:00 -0600

    Fonte-Awesome added, Home Page design finished

--- SchuvarItinerary/Controllers/HomeController.cs
+++ SchuvarItinerary/Controllers/HomeController.cs
@@ -1,31 +1,32 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SchuvarItinerary.Models;
 
 namespace SchuvarItinerary.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly SchuvarItineraryContext context;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(SchuvarItineraryContext context)
+  {
+    this.context = context;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public async Task<IActionResult> Index()
+  {
+    return View(await context.Aerolineas.ToListAsync());
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
 }
