2ed1b4feb6d54c2f9b8dcdd324b5a51b6501ec78
Daniel Flores <dflores2t@gmail.com>
2022-12-06 15:55:13 -0600

    list airlines, crud airlines, validations, style added

---
+++ SchuvarItinerary/Models/ViewModels/ViewAirLine.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using SchuvarItinerary.Models;
+namespace SchuvarItinerary.Models.ViewModels;
+
+public class ViewAirLine
+{
+  public ViewAirLine()
+  { }
+  public ViewAirLine(Aerolinea model)
+  {
+    IdAerolinea = model.IdAerolinea;
+    AerolineaName = model.AerolineaName;
+    AeroDescription = model.AeroDescription;
+    IsDeleted = model.IsDeleted;
+  }
+  [Display(Name = "Id")]
+  public int IdAerolinea { get; set; }
+  [Display(Name = "Short Name")]
+  [StringLength(maximumLength: 2, MinimumLength = 2, ErrorMessage = "Is necesary two caracter for short name!")]
+  [Required(ErrorMessage = "This field is required!")]
+  public string? AerolineaName { get; set; }
+  [Display(Name = "Aero Line Description")]
+  [Required(ErrorMessage = "This field is required!")]
+  [StringLength(maximumLength: 50, MinimumLength = 5, ErrorMessage = "String characters between 5 and 50")]
+  public string? AeroDescription { get; set; }
+  /// <summary>
+  ///IsDelete return true if record is deleted
+  /// </summary>
+  public bool? IsDeleted { get; set; } = false;
+}
