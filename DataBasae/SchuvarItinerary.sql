116c41d4dc3d0a234556ca49e362d447834bc427
Daniel Flores <dflores2t@gmail.com>
2023-02-10 13:12:29 -0600

    database query on postgresql.

--- DataBasae/SchuvarItinerary.sql
+++ DataBasae/SchuvarItinerary.sql
@@ -9,34 +9,44 @@ Create DATABASE SchuvarItinerary
 go
 use SchuvarItinerary
 go
-create TABLE Customer
+create TABLE customer
 (
-  IdCustomer INT IDENTITY(1,1),
-  FullName VARCHAR(100) not null,
-  Contact INT not null
-    CONSTRAINT pk_id_customer PRIMARY KEY(IdCustomer),
+  customer_id INT IDENTITY(1,1),
+  customer_fullname VARCHAR(100) not null,
+  customer_phone INT not null,
+  customer_isdeleted bit,
+  customer_dateup datetime,
+  customer_datemodify datetime
+    CONSTRAINT pk_id_customer PRIMARY KEY(customer_id),
 )
+--create index on customer_phone column
+create index idx_cusmoer_phone on customer(customer_phone)
 go
-create TABLE Aerolinea
+
+create TABLE aerolinea
 (
-  IdAerolinea INT IDENTITY(1,1),
-  AerolineaName VARCHAR(2) not null,
-  AeroDescription VARCHAR(50) not null,
-  isDeleted BIT,
+  aerolinea_id INT IDENTITY(1,1),
+  aerolinea_shortname VARCHAR(2) not null,
+  aerolinea_fullname VARCHAR(50) not null,
+  aerolinea_isdeleted BIT,
+  aerolinea_dateup datetime,
+  aeroliena_datemodify datetime,
   constraint pk_Id_aerolinea PRIMARY KEY(IdAerolinea)
 )
 GO
-create table FlyCustomer
+create table flycustomer
 (
-  IdFly INT IDENTITY(1,1),
-  IdCustomer INT not null,
-  IdAerolinea INT not null,
-  Route VARCHAR(20) not null,
-  Localizer VARCHAR(10) not null,
-  Departures DATE not null,
-  Arrivals DATE not null
-    CONSTRAINT pk_IdFly_FlyCustomer primary KEY(IdFly),
-  constraint fk_IdCustomer_FlyCustomer_Customer FOREIGN KEY(IdCustomer) REFERENCES Customer(IdCustomer),
-  CONSTRAINT fk_IdAerolinea_FlyCustomer_Aerolinea FOREIGN KEY(IdAerolinea) REFERENCES Aerolinea(IdAerolinea)
+  flycustomer_id INT IDENTITY(1,1),
+  flycustomer_idcustomer INT not null,
+  flycustomer_idaerolinea INT not null,
+  flycustomer_route VARCHAR(20) not null,
+  flycustomer_localizer VARCHAR(10) not null,
+  flycustomer_departures DATE not null,
+  flycustomer_arrivals DATE not null,
+  flycustomer_dateup datetime,
+  flycustomer_datemodify datetime
+    CONSTRAINT pk_IdFly_FlyCustomer primary KEY(flycustomer_id),
+  constraint fk_IdCustomer_FlyCustomer_Customer FOREIGN KEY(flycustomer_idcustomer) REFERENCES customer(customer_id),
+  CONSTRAINT fk_IdAerolinea_FlyCustomer_Aerolinea FOREIGN KEY(flycustomer_idaerolinea) REFERENCES aerolinea(aerolinea_id)
 
 )
--- DataBasae/SchuvarItineraryP.sql
+++ DataBasae/SchuvarItineraryP.sql
@@ -24,15 +24,75 @@ CREATE DATABASE schuvaritinerary
     WITH
     OWNER = schuvardb
     ENCODING = 'UTF8'
+    TABLESPACE = sc_primary
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
 COMMENT ON DATABASE schuvaritinerary
-    IS 'database to managment custemore itinerary';
+    IS 'databaso to management customers itinerary';
 --create schema
 CREATE SCHEMA schu
     AUTHORIZATION schuvardb;
 set search_path to schu, public;
 show search_path;
 
-
+--TABLES CREATION SECTION
+--create table customer
+--create table customer
+drop table if exists schu.customer;
+CREATE TABLE schu.customer
+(
+    customer_id integer GENERATED ALWAYS AS IDENTITY,
+    customer_fullname character varying(100) NOT NULL,
+    customer_phone character varying(9) NOT NULL,
+	customer_isdeleted boolean default false,
+	customer_dateup timestamp default now(),
+	customer_datemodify timestamp default now(),
+    PRIMARY KEY (customer_id),
+    CONSTRAINT unq_phone UNIQUE (customer_phone),
+    CONSTRAINT chk_pone CHECK (customer_phone ~ '^[0-9]{4}-[0-9]{4}')
+);
+--index on customer_phone
+create index idx_customer_phone
+	on schu.customer(customer_phone);
+--table owner -
+ALTER TABLE IF EXISTS schu.customer
+    OWNER to schuvardb;
+--create table aerolinea.
+drop table if exists schu.aerolinea;
+create table schu.aerolinea(
+	aerolinea_id integer generated always as identity,
+	aerolinea_shortname varchar(2) not null,
+	aerolinea_fullname varchar(50) not null,
+	aerolinea_idDeleted boolean default  false,
+	aerolinea_dateup timestamp default now(),
+	aerolinea_datemodify timestamp default now(),
+	primary key(aerolinea_id)
+);
+--table owner
+alter table if exists schu.aerolinea
+	OWNER to schuvardb;
+--table flycustomer
+drop table if exists schu.flycustomer;
+create table schu.flycustomer(
+	flycustomer_id integer generated always as identity,
+	flycustomer_idcustomer int not null,
+	flycustomer_idaerolinea int not null,
+	flycustomer_route varchar(20) not null,
+	flycustomer_localyzer varchar(10) not null,
+	flycustomer_departure date default current_date,
+	flycustomer_arrivals date  default current_date,
+	flycustomer_isdeleted boolean default false,
+	flycustomer_dateup timestamp default now(),
+	flycustomer_datemodify timestamp default now(),
+	primary key(flycustomer_id),
+	foreign key(flycustomer_idcustomer)
+		references schu.customer(customer_id)
+		on update cascade on delete cascade,
+	foreign key(flycustomer_idaerolinea)
+		references schu.aerolinea(aerolinea_id)
+		on update cascade on delete cascade	
+);
+--set table owner
+alter table if exists schu.flycustomer
+	owner to schuvardb;
